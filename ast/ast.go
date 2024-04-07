e4d2d38b4131b1f453658cf36eabc61e57ddec1c
Han-Zihang <han.zihang@outlook.com>
2024-04-07 18:23:04 +0800

    expression_integer

--- ast/ast.go
+++ ast/ast.go
@@ -1,7 +1,6 @@
 package ast
 
 import (
-	"monkey-interpreter/token"
 	"strings"
 )
 
@@ -51,20 +50,3 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
-
-type Identifier struct {
-	Token token.Token // token.IDENT
-	Value string
-}
-
-// Identifier
-// Monkey 程序中有些标识符会产生值
-// 为了减少 AST 中各种类型节点的数量
-// 在变量绑定中使用 Identifier 表示名称
-// 之后在表达表达式中的标识符的时候
-// 可以复用 Identifier 节点
-func (i *Identifier) expressionNode() {}
-
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-
-func (i *Identifier) String() string { return i.Value }
