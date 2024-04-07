a373ea4f064035a614cd1be3b02b27a13348527b
Han-Zihang <han.zihang@outlook.com>
2024-04-07 15:10:43 +0800

    let_statement

--- ast/ast.go
+++ ast/ast.go
@@ -33,6 +33,12 @@ type Identifier struct {
 	Value string
 }
 
+// Identifier
+// Monkey 程序中有些标识符会产生值
+// 为了减少 AST 中各种类型节点的数量
+// 在变量绑定中使用 Identifier 表示名称
+// 之后在表达表达式中的标识符的时候
+// 可以复用 Identifier 节点
 func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
