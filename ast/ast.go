64869d9017391ee804900726349b3b4ebfdc0191
Han Zihang <57861109+Han-Zihang@users.noreply.github.com>
2024-04-07 16:31:50 +0800

    Feat/2.6 expression (#9)
    * expression

--- ast/ast.go
+++ ast/ast.go
@@ -1,23 +1,47 @@
 package ast
 
-import "monkey-interpreter/token"
+import (
+	"monkey-interpreter/token"
+	"strings"
+)
 
 type Node interface {
 	TokenLiteral() string // 用来调试
+	String() string       // 用来调试, 打印比较 AST 节点
 }
 
+// Statement 语句
 type Statement interface {
 	Node
 	statementNode() // 占位, 编译检查
 }
 
+// Expression 表达式
 type Expression interface {
 	Node
 	expressionNode() // 占位, 编译检查
 }
 
+// Program 程序
+// 由一系列 Statement 语句组成
+// Statement 和 Expression 的区别是
+// Statement 语句不会产生值
+// Expression 表达式会产生值
+// 比如 let x = 5;
+// let x = 5; 不会产生值
+// 5 会产生值, 就是 5
+// return 5; 不会产生值
+// add(5, 5) 会产生值
 type Program struct {
 	Statements []Statement
+}
+
+func (p *Program) String() string {
+	var out strings.Builder // strings.Builder 性能比原文中 bytes.Buffer 略快约 10%
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 func (p *Program) TokenLiteral() string {
@@ -42,3 +66,5 @@ type Identifier struct {
 func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string { return i.Value }
