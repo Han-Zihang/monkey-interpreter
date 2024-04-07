aca3a198825acb82f2699debcf733f2685da7647
Han-Zihang <han.zihang@outlook.com>
2024-04-07 15:23:08 +0800

    let_statement

---
+++ parser/parser_0.go
@@ -0,0 +1,54 @@
+package parser
+
+import (
+	"fmt"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+
+	errors []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	// 设置 curToken 和 peekToken
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+func (p *Parser) peekError(t token.Type) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) curTokenIs(t token.Type) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.peekToken.Type == t
+}
